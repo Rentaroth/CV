8e15ee88f89c76720137c38ff5760790e5f4b6a7
Antonio <antonio.tabares@outlook.es>
2024-01-07 19:35:47 -0500

    Build function freed and fixed, ready to build early production

--- cv-frontend/vite.config.js
+++ cv-frontend/vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { babel } from '@rollup/plugin-babel';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  loadEnv(mode, process.cwd(), '');
-
+export default ({ command, mode }) => {
+  loadEnv(mode, '');
+  if(command === 'build') {
+    return defineConfig({
+      plugins: [react(), babel({ babelHelpers: 'bundled'})],
+      build: {
+        outDir: "./dist/"
+      }
+    })
+  }
   return defineConfig({
-    plugins: [react(), tailwindcss(), autoprefixer()],
+    plugins: [react(), babel({ babelHelpers: 'bundled'}), tailwindcss(), autoprefixer()],
+    build: {
+      outDir: "./dist/"
+    }
   })
 }
