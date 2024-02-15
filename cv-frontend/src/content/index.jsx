573e8f5f51bf4acff17c242167b545961665b9f7
Rentaroth <antonio.tabares@outlook.es>
2024-02-15 13:53:53 -0500

    Fixed bad code line

--- cv-frontend/src/content/index.jsx
+++ cv-frontend/src/content/index.jsx
@@ -1,9 +1,7 @@
 import { Schema1 } from "./schema_1/schema_1";
-import { Schema2 } from "./schema_2/schema_2";
 import { Loading } from "./loading/loading";
 
 export {
   Schema1,
-  Schema2,
   Loading,
 }
